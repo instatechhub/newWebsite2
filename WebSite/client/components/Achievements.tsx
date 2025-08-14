cdd7715d7b28d49ee6efba867923f04816469b21
instatechhub <instahub902@gmail.com>
2025-08-14 19:56:14 +0530

    new nwe

--- WebSite/client/components/Achievements.tsx
+++ WebSite/client/components/Achievements.tsx
@@ -22,7 +22,7 @@ const skills = [
   },
   {
     name: "Email Support",
-    percentage: 5,
+    percentage: 10,
     icon: (
       <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
         <path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 4l-8 5-8-5V6l8 5 8-5z" />
